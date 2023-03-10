f27b36c694f52c7fd568b1a8d820cb2328dfdba9
rubberduckk <kim.ye3un@gmail.com>
2023-03-10 10:49:51 +0900

    complete #11382

---
+++ 20230310/20230310/boj_11382.cpp
@@ -0,0 +1,19 @@
+//
+//  boj_11382.cpp
+//  20230310
+//
+//  Created by 김예은 on 2023/03/10.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    long long a, b, c;
+    cin >> a >> b >> c;
+    cout << a+b+c << endl;
+}
+
